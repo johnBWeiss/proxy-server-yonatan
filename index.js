08730879c846a67575593e4107dc3126ff3e54e5
johnBWeiss <yoniw7@gmail.com>
2023-08-05 12:10:50 +0300

    axios call

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 8081;
+require('dotenv').config();
+
+const baseURL = 'http://dataservice.accuweather.com'
+const apiKey = process.env.API_KEY;
 
 app.get('/', async (req, res) => {
   res.send('hi');
 
   try {
-
+    let response = await axios(`${baseURL}/forecasts/v1/daily/5day/${payload.cityCode}?apiKey=${apiKey}&language=en-us`)
+    console.log(response);
 
   } catch (error) {
     res.status(400).send('Error while getting list of repositories');
