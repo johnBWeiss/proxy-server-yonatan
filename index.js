10d0f0ffc3334e21979bc3fd0fb9036ba4a9637d
johnBWeiss <yoniw7@gmail.com>
2023-08-05 12:49:27 +0300

    add all api routes

--- index.js
+++ index.js
@@ -7,12 +7,42 @@ require('dotenv').config();
 
 
 const baseURL = 'http://dataservice.accuweather.com'
-const apiKey = process.env.API_KEY;
+const apiKey = process.env.API_KEY; const searchByTextURL = `${baseURL}/locations/v1/cities/autocomplete`
+const geoPositionURL = `${baseURL}/locations/v1/cities/geoposition/search`
 
-app.get('/', async (req, res) => {
+app.get('/getFiveDays', async (req, res) => {
 
   try {
     let response = await axios(`${baseURL}/forecasts/v1/daily/5day/215854?apikey=${apiKey}`)
+    res.json(response?.data);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+app.get('/searchText/:text', async (req, res) => {
+  const searchText = req.params.text;
+  try {
+    let response = await axios(`${searchByTextURL}?apikey=${apiKey}&q=${searchText}&language=en-us`)
+    res.json(response?.data);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+app.get('/getSingleCity/:cityCode', async (req, res) => {
+  const cityCode = req.params.cityCode;
+  try {
+    let response = await axios(`${baseURL}/currentconditions/v1/${cityCode}?apikey=${apiKey}&language=en-us`)
+    res.json(response?.data);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+app.get('/getGeoPosition/:lat/:long', async (req, res) => {
+  const lat = req.params.lat;
+  const long = req.params.long;
+  try {
+    let response = await axios(`${geoPositionURL}?apikey=${apiKey}&q=${lat}%2C${long}&language=en-us&toplevel=false`
+    )
     res.json(response?.data);
   } catch (error) {
     res.status(400).json({ error: error.message });
