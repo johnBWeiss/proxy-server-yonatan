4be70b5c72f317f7c8d35a913520509b7707ab59
johnBWeiss <yoniw7@gmail.com>
2023-08-07 00:03:24 +0300

    updted keys

--- index.js
+++ index.js
@@ -12,36 +12,10 @@ const baseURL = 'http://dataservice.accuweather.com'
 const searchByTextURL = `${baseURL}/locations/v1/cities/autocomplete`
 const geoPositionURL = `${baseURL}/locations/v1/cities/geoposition/search`
 
-const apiKeysArray = ['IeogV01qgqGpHm1XxALIFB1JAtbxBs7E', 'n3dsU8isX5GGSbrRWMrxOxGK7Wb3TgIQ', '9x0oehpt46AI4aK2veZLfftoKZfi3AR8', 'R2wMZwBSVoGRpGVartAWpcPs7Oaz9zsy']
-
-const getRandomKey = (apiKeysArray) => {
-  const randomIndex = Math.floor(Math.random() * apiKeysArray.length);
-  return apiKeysArray[randomIndex];
-}
-
-
-app.get('/', async (req, res) => {
-  console.log(apiKey);
-  try {
-
-  } catch (error) {
-    console.log(error.message);
-    res.status(400).json({ error: error.message });
-  }
-});
-
-
 
 app.get('/getFiveDays/:cityCode', async (req, res) => {
   const cityCode = req.params.cityCode;
-  // const apiKey = 'CjfrofJZlG4PhsDzPl4O6ErKHBmGE77F'
-  const apiKey = 'LuXt1Rlu5zBXvppMbYi5Ms8kB0X1Tfvc'
-
-  //const apiKey = getRandomKey(apiKeysArray)
-  // const apiKey = 's0jaA8ot7GpKr1vrvB2U7l1Hzvebkt35'
-  console.log(apiKey);
-
-
+  const apiKey = 'GBOpYKJGyAyD8E5W1mI86b3pA3HnbC5O'
   try {
     let response = await axios(`${baseURL}/forecasts/v1/daily/5day/${cityCode}?apikey=${apiKey}`)
     res.json(response?.data);
@@ -50,15 +24,9 @@ app.get('/getFiveDays/:cityCode', async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 });
+
 app.get('/searchText/:text', async (req, res) => {
-  const apiKey = 'LuXt1Rlu5zBXvppMbYi5Ms8kB0X1Tfvc'
-
-  //const apiKey = 'CjfrofJZlG4PhsDzPl4O6ErKHBmGE77F'
-  // const apiKey = getRandomKey(apiKeysArray)
-  // const apiKey = 's0jaA8ot7GpKr1vrvB2U7l1Hzvebkt35'
-
-  console.log(apiKey);
-
+  const apiKey = 'GBOpYKJGyAyD8E5W1mI86b3pA3HnbC5O'
   const searchText = req.params.text;
   try {
     let response = await axios(`${searchByTextURL}?apikey=${apiKey}&q=${searchText}&language=en-us`)
@@ -68,14 +36,9 @@ app.get('/searchText/:text', async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 });
+
 app.get('/getSingleCity/:cityCode', async (req, res) => {
-  //const apiKey = getRandomKey(apiKeysArray)
-  // const apiKey = 's0jaA8ot7GpKr1vrvB2U7l1Hzvebkt35'
-  //const apiKey = 'CjfrofJZlG4PhsDzPl4O6ErKHBmGE77F'
-  const apiKey = 'LuXt1Rlu5zBXvppMbYi5Ms8kB0X1Tfvc'
-
-  console.log(apiKey);
-
+  const apiKey = 'GBOpYKJGyAyD8E5W1mI86b3pA3HnbC5O'
   const cityCode = req.params.cityCode;
   try {
     let response = await axios(`${baseURL}/currentconditions/v1/${cityCode}?apikey=${apiKey}&language=en-us`)
@@ -85,14 +48,9 @@ app.get('/getSingleCity/:cityCode', async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 });
+
 app.get('/getGeoPosition/:lat/:long', async (req, res) => {
-  //const apiKey = getRandomKey(apiKeysArray)
-  // const apiKey = 's0jaA8ot7GpKr1vrvB2U7l1Hzvebkt35'
-  //const apiKey = 'CjfrofJZlG4PhsDzPl4O6ErKHBmGE77F'
-  const apiKey = 'LuXt1Rlu5zBXvppMbYi5Ms8kB0X1Tfvc'
-
-  console.log(apiKey);
-
+  const apiKey = 'GBOpYKJGyAyD8E5W1mI86b3pA3HnbC5O'
   const lat = req.params.lat;
   const long = req.params.long;
   try {
