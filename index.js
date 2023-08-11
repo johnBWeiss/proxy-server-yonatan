a4abe81cbb9ecde28155e88ba4d9daea73c2060f
johnBWeiss <yoniw7@gmail.com>
2023-08-11 22:10:27 +0300

    random jey for wednesday interview

--- index.js
+++ index.js
@@ -2,20 +2,26 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const cors = require('cors');
-
-
 const PORT = process.env.PORT || 8081;
+
 require('dotenv').config();
 app.use(cors('*'));
 
 const baseURL = 'http://dataservice.accuweather.com'
 const searchByTextURL = `${baseURL}/locations/v1/cities/autocomplete`
 const geoPositionURL = `${baseURL}/locations/v1/cities/geoposition/search`
+const apiKeysArray = ['LuXt1Rlu5zBXvppMbYi5Ms8kB0X1Tfvc', 'CjfrofJZlG4PhsDzPl4O6ErKHBmGE77F', 'GBOpYKJGyAyD8E5W1mI86b3pA3HnbC5O', 'IeogV01qgqGpHm1XxALIFB1JAtbxBs7E', 'n3dsU8isX5GGSbrRWMrxOxGK7Wb3TgIQ', '9x0oehpt46AI4aK2veZLfftoKZfi3AR8', 'R2wMZwBSVoGRpGVartAWpcPs7Oaz9zsy']
 
+const getRandomKey = (apiKeysArray) => {
+  const randomIndex = Math.floor(Math.random() * apiKeysArray.length);
+  return apiKeysArray[randomIndex];
+}
 
 app.get('/getFiveDays/:cityCode', async (req, res) => {
   const cityCode = req.params.cityCode;
-  const apiKey = 'GBOpYKJGyAyD8E5W1mI86b3pA3HnbC5O'
+  const apiKey = getRandomKey(apiKeysArray);
+  console.log(apiKey);
+
   try {
     let response = await axios(`${baseURL}/forecasts/v1/daily/5day/${cityCode}?apikey=${apiKey}`)
     res.json(response?.data);
@@ -26,7 +32,8 @@ app.get('/getFiveDays/:cityCode', async (req, res) => {
 });
 
 app.get('/searchText/:text', async (req, res) => {
-  const apiKey = 'GBOpYKJGyAyD8E5W1mI86b3pA3HnbC5O'
+  const apiKey = getRandomKey(apiKeysArray);
+  console.log(apiKey);
   const searchText = req.params.text;
   try {
     let response = await axios(`${searchByTextURL}?apikey=${apiKey}&q=${searchText}&language=en-us`)
@@ -38,7 +45,9 @@ app.get('/searchText/:text', async (req, res) => {
 });
 
 app.get('/getSingleCity/:cityCode', async (req, res) => {
-  const apiKey = 'GBOpYKJGyAyD8E5W1mI86b3pA3HnbC5O'
+  const apiKey = getRandomKey(apiKeysArray);
+  console.log(apiKey);
+
   const cityCode = req.params.cityCode;
   try {
     let response = await axios(`${baseURL}/currentconditions/v1/${cityCode}?apikey=${apiKey}&language=en-us`)
@@ -50,7 +59,9 @@ app.get('/getSingleCity/:cityCode', async (req, res) => {
 });
 
 app.get('/getGeoPosition/:lat/:long', async (req, res) => {
-  const apiKey = 'GBOpYKJGyAyD8E5W1mI86b3pA3HnbC5O'
+  const apiKey = getRandomKey(apiKeysArray);
+  console.log(apiKey);
+
   const lat = req.params.lat;
   const long = req.params.long;
   try {
